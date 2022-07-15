fcae95d4bbfbe956a91e4e681b66d9b8e635f34e
maemon4095 <maemon4095@users.noreply.github.com>
2022-07-15 16:53:04 +0000

    deploy: f7a491d96f87c86e534ef75b968abd022c903722

--- service_worker.js
+++ service_worker.js
@@ -1,21 +1,22 @@
 var version = 'yew-trial 0.0.0';
 var files = [
     './',
-    './index.html'
+    './index.html',
+    './images/y.svg'
 ];
 
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open(version).then(function(e){
+        caches.open(version).then(function (e) {
             return cache.addAll(files);
         })
     );
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
     e.respondWith(
-        caches.match(e.request).then(function(response) { 
+        caches.match(e.request).then(function (response) {
             return response || fetch(e.request)
         })
     );
